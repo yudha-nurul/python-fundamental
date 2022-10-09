92cc365d20fc2a7d10114ccc1c0a9b660fdd6c42
Yudha Nurul Alfian <67511620+yudha-nurul@users.noreply.github.com>
2022-10-09 21:44:57 +0700

    update sintaksis_dasar_ perulangan_while.py

--- sintaksis_dasar_ perulangan_while.py
+++ sintaksis_dasar_ perulangan_while.py
@@ -2,3 +2,16 @@
 program perulangan membaca buku
 """
 # perulangan dengan while
+perulangan dengan for
+jumlah_buku = 100
+buku_yang_sudah_dibaca = 0
+print(f'jumlah buku yang sudah dibaca adalah {buku_yang_sudah_dibaca}')
+for buku_yang_sudah_dibaca in range (1, jumlah_buku + 1):
+    print(f'buku ke {buku_yang_sudah_dibaca} sudah dibaca')
+print(f'jumlah buku yang sudah dibaca {buku_yang_sudah_dibaca}')
+print('selesai,,,,')
+
+
+
+
+
