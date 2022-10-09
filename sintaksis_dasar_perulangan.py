bce48d29c4a437882afec3763df0d4d60d606328
yudha nurul alfian <yudhanurulalfian@gmail.com>
2022-10-09 06:46:24 -0700

    perulangan dengan for

--- sintaksis_dasar_perulangan.py
+++ sintaksis_dasar_perulangan.py
@@ -2,7 +2,7 @@
 program perulangan membaca buku
 """
 # perulangan dengan for
-jumlah_buku = 10
+jumlah_buku = 100
 buku_yang_sudah_dibaca = 0
 print(f'jumlah buku yang sudah dibaca adalah {buku_yang_sudah_dibaca}')
 for buku_yang_sudah_dibaca in range (1, jumlah_buku + 1):
