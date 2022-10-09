93900325943a149ee01653e2a40a9bf34e213b0c
yudha nurul alfian <yudhanurulalfian@gmail.com>
2022-10-09 03:25:43 -0700

    materi pertama

---
+++ sintaksis_dasar_percabangan.py
@@ -0,0 +1,28 @@
+# Sequential
+print('Ibu berkata, "belikan ibu sebotol susu"')
+print('Budi menjawab, "Baik bu,, apa yang harus saya lakukan ditoko?"')
+print('Ibu menjawab, "Beli satu botol susu"')
+print('maka Budi Berangkat ke Toko')
+print('dan mulai berbelanja')
+
+# Percabangan, contoh 1
+jumlah_botol_susu_ditoko = 0
+print(f'Budi melihat bahwa ada {jumlah_botol_susu_ditoko} botol susu di toko')
+if jumlah_botol_susu_ditoko > 0:
+    print('maka Budi membeli 1 botol susu')
+else:
+    print('maka Budi tidak jadi membeli susu')
+print('dan kembali pulang ke rumah')
+print('=' * 50)
+print('=' * 50)
+
+# Percabangan, contoh 2, dari joke programmer
+"""
+-ibu berkata: tolong pergi ketoko dan beli 1 botol susu, jika disana ada telur, bawa 6.
+-lalu saya kembali membawa 6 botol susu.
+-ibu berkata: kenapa kamu bawa 6 botol susu?
+-karena ditoko ada telur. :-)
+tugas: dari dialog diatas, buat flowchart yg benar dan tulis programnya 
+"""
+print("ibu berkata: tolong pergi ketoko dan beli satu botol susu")
+print("jika disana ")
