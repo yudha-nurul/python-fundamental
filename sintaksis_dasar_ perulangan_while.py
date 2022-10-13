1c0035c4b5d6fe0207e42ef2e14d40a1e09d6a0f
yudha alfian <yudhanurulalfian@gmail.com>
2022-10-13 10:17:18 +0700

    perulangan dengan while

--- sintaksis_dasar_ perulangan_while.py
+++ sintaksis_dasar_ perulangan_while.py
@@ -2,10 +2,11 @@
 program perulangan membaca buku
 """
 # perulangan dengan while
-jumlah_buku = 100
+jumlah_buku = 10
 buku_yang_sudah_dibaca = 0
 print(f'jumlah buku yang sudah dibaca adalah {buku_yang_sudah_dibaca}')
-for buku_yang_sudah_dibaca in range (1, jumlah_buku + 1):
+while buku_yang_sudah_dibaca < jumlah_buku:
+    buku_yang_sudah_dibaca = buku_yang_sudah_dibaca + 1
     print(f'buku ke {buku_yang_sudah_dibaca} sudah dibaca')
 print(f'jumlah buku yang sudah dibaca {buku_yang_sudah_dibaca}')
 print('selesai,,,,')
