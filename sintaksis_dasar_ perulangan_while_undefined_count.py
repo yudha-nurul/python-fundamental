856c379c7779a5027eed5e2bee88d5a6f22042d2
yudha alfian <yudhanurulalfian@gmail.com>
2022-10-13 15:41:02 +0700

    perulangan while undefined count

---
+++ sintaksis_dasar_ perulangan_while_undefined_count.py
@@ -0,0 +1,18 @@
+"""
+program perulangan membaca buku  sampai paham
+"""
+# perulangan dengan while
+jumlah_buku = 10
+buku_yang_sudah_dibaca = 0
+print(f'jumlah buku yang sudah dibaca adalah {buku_yang_sudah_dibaca}')
+while buku_yang_sudah_dibaca < jumlah_buku:
+    buku_yang_sudah_dibaca = buku_yang_sudah_dibaca + 1
+    print(f'buku ke {buku_yang_sudah_dibaca} sudah dibaca')
+print(f'jumlah buku yang sudah dibaca {buku_yang_sudah_dibaca}')
+print('selesai,,,,')
+
+
+
+
+
+
