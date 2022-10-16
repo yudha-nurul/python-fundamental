ffe2c1a828d0e5c40d91475327eceb9493da7ec6
yudha nurul alfian <yudhanurulalfian@gmail.com>
2022-10-16 00:41:08 -0700

    perulangan while dengan variable henti, rename to english

--- sintaksis_dasar_ perulangan_while_undefined_count.py
+++ sintaksis_dasar_ perulangan_while_undefined_count.py
@@ -2,22 +2,22 @@
 program perulangan membaca buku  sampai paham
 """
 # perulangan dengan while dengan penanda henti
-jumlah_buku = 10
-jumlah_baca = 0
-jumlah_paham = 0
-print(f'jumlah buku adalah {jumlah_buku}')
-print(f'jumlah buku yang sudah dibaca adalah {jumlah_paham}')
-while jumlah_baca < jumlah_buku + 3:
-    jumlah_baca = jumlah_baca + 1
-    if jumlah_paham == 8:
-        print(f'buku ke {jumlah_paham + 1} belum paham')
+book_count = 10
+read_count = 0
+understood_count = 0
+print(f'jumlah buku adalah {book_count}')
+print(f'jumlah buku yang sudah dibaca adalah {understood_count}')
+while read_count < book_count + 3:
+    read_count = read_count + 1
+    if understood_count == 8:
+        print(f'buku ke {understood_count + 1} belum paham')
     else:
-        jumlah_paham = jumlah_paham + 1
-        print(f'buku ke {jumlah_paham} sudah dibaca dan dipahami')
+        understood_count = understood_count + 1
+        print(f'buku ke {understood_count} sudah dibaca dan dipahami')
 
-print(f'jumlah buku yang sudah dibaca dan dipahami adalah {jumlah_paham}')
-if jumlah_paham == jumlah_buku:
+print(f'jumlah buku yang sudah dibaca dan dipahami adalah {understood_count}')
+if understood_count == book_count:
     print('semua buku sudah saya baca dan pahami bu,,,')
 else:
-    print(f'tidak semua buku saya pahami, saya hanya paham {jumlah_paham} buku')
+    print(f'tidak semua buku saya pahami, saya hanya paham {understood_count} buku')
 print('selesai,,,,')
