404fa67e51e6f9d14c6702f65d1af6b5baa92224
yudha nurul alfian <yudhanurulalfian@gmail.com>
2022-10-16 00:37:47 -0700

    perulangan while dengan variable henti

--- sintaksis_dasar_ perulangan_while_undefined_count.py
+++ sintaksis_dasar_ perulangan_while_undefined_count.py
@@ -1,18 +1,23 @@
 """
 program perulangan membaca buku  sampai paham
 """
-# perulangan dengan while
+# perulangan dengan while dengan penanda henti
 jumlah_buku = 10
-buku_yang_sudah_dibaca = 0
-print(f'jumlah buku yang sudah dibaca adalah {buku_yang_sudah_dibaca}')
-while buku_yang_sudah_dibaca < jumlah_buku:
-    buku_yang_sudah_dibaca = buku_yang_sudah_dibaca + 1
-    print(f'buku ke {buku_yang_sudah_dibaca} sudah dibaca')
-print(f'jumlah buku yang sudah dibaca {buku_yang_sudah_dibaca}')
+jumlah_baca = 0
+jumlah_paham = 0
+print(f'jumlah buku adalah {jumlah_buku}')
+print(f'jumlah buku yang sudah dibaca adalah {jumlah_paham}')
+while jumlah_baca < jumlah_buku + 3:
+    jumlah_baca = jumlah_baca + 1
+    if jumlah_paham == 8:
+        print(f'buku ke {jumlah_paham + 1} belum paham')
+    else:
+        jumlah_paham = jumlah_paham + 1
+        print(f'buku ke {jumlah_paham} sudah dibaca dan dipahami')
+
+print(f'jumlah buku yang sudah dibaca dan dipahami adalah {jumlah_paham}')
+if jumlah_paham == jumlah_buku:
+    print('semua buku sudah saya baca dan pahami bu,,,')
+else:
+    print(f'tidak semua buku saya pahami, saya hanya paham {jumlah_paham} buku')
 print('selesai,,,,')
-
-
-
-
-
-
