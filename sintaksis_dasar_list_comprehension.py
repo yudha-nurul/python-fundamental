b982fff167a315e522f5117744e960506a183c87
yudha nurul alfian <yudhanurulalfian@gmail.com>
2022-11-01 07:07:46 -0700

    list comprehension

--- sintaksis_dasar_list_comprehension.py
+++ sintaksis_dasar_list_comprehension.py
@@ -1,13 +1,35 @@
 # ==========materi list comprehension
-print('\n>>>>>List comprehension')
+print('\n>>>>>Materi List comprehension')
+daftar_buku = ['fisika', 'biologi', 'matematika', 'kimia']
+print(daftar_buku)
 
-# perintah del dengan list comprehension
-print('\nperintah del dengan list comprehension')
-# menghapus index ke 1 dalam list
+# >>perintah del dengan list comprehension
+print('\n>>perintah del dengan list comprehension')
+daftar_buku = ['fisika', 'biologi', 'matematika', 'kimia']
+del daftar_buku[0]  # menghapus index ke 1 dalam list
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
 # menghapus semua elemen dalam index daftar buku
-# menghapus index ke 1 dan 2 (0 & 1)
+print('\n>>menghapus semua elemen dalam index daftar buku')
+daftar_buku = ['fisika', 'biologi', 'matematika', 'kimia']
+del daftar_buku[:]  # menghapus semua elemen dalam index daftar buku
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
 
-# perintah del dengan list comprehension START : END : STEP
+# menghapus index ke 1 dan 2 (0 & 1)
+print('\n>>menghapus index ke 1 dan 2 (0 & 1)')
+daftar_buku = ['fisika', 'biologi', 'matematika', 'kimia']
+del daftar_buku[0:2]  # menghapus index ke 1 dan 2 (0 & 1)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+# >>perintah del dengan list comprehension START : END : STEP
+print('\n>>perintah del dengan list comprehension START : END : STEP')
+daftar_buku = ['fisika', 'biologi', 'matematika', 'kimia']
+del daftar_buku[0::2]  # perintah del dengan list comprehension START : END : STEP
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
 
 # membuat list dari list yang sudah ada
 
