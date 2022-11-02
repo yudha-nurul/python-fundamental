c7f2140bbfbbffe01c29b8f5e999fd3da30f2a22
yudha nurul alfian <yudhanurulalfian@gmail.com>
2022-11-01 17:59:35 -0700

    list comprehension

--- sintaksis_dasar_list_comprehension.py
+++ sintaksis_dasar_list_comprehension.py
@@ -3,42 +3,68 @@ print('\n>>>>>Materi List comprehension')
 daftar_buku = ['fisika', 'biologi', 'matematika', 'kimia']
 print(daftar_buku)
 
-# >>perintah del dengan list comprehension
-print('\n>>perintah del dengan list comprehension')
+# >>1. perintah del dengan list comprehension
+print('\n>>1. perintah del dengan list comprehension')
 daftar_buku = ['fisika', 'biologi', 'matematika', 'kimia']
+print(daftar_buku)
 del daftar_buku[0]  # menghapus index ke 1 dalam list
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
-# menghapus semua elemen dalam index daftar buku
-print('\n>>menghapus semua elemen dalam index daftar buku')
+# 2. menghapus semua elemen dalam index daftar buku
+print('\n>>2. menghapus semua elemen dalam index daftar buku')
 daftar_buku = ['fisika', 'biologi', 'matematika', 'kimia']
+print(daftar_buku)
 del daftar_buku[:]  # menghapus semua elemen dalam index daftar buku
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
-# menghapus index ke 1 dan 2 (0 & 1)
-print('\n>>menghapus index ke 1 dan 2 (0 & 1)')
+# 3. menghapus index ke 1 dan 2 (0 & 1)
+print('\n>>3. menghapus index ke 1 dan 2 (0 & 1)')
 daftar_buku = ['fisika', 'biologi', 'matematika', 'kimia']
+print(daftar_buku)
 del daftar_buku[0:2]  # menghapus index ke 1 dan 2 (0 & 1)
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
-# >>perintah del dengan list comprehension START : END : STEP
-print('\n>>perintah del dengan list comprehension START : END : STEP')
+# >>4. perintah del dengan list comprehension START : END : STEP
+print('\n>>4. perintah del dengan list comprehension START : END : STEP')
 daftar_buku = ['fisika', 'biologi', 'matematika', 'kimia']
+print(daftar_buku)
 del daftar_buku[0::2]  # perintah del dengan list comprehension START : END : STEP
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
 
-# membuat list dari list yang sudah ada
+# 5. membuat list dari list yang sudah ada
+print('\n5. membuat list dari list yang sudah ada')
+daftar_buku = ['fisika', 'biologi', 'matematika', 'kimia']
+print(daftar_buku)
+daftar_buku_baru = daftar_buku[:]  # 6. harus ditambah ini ([:]) untuk benar2 baru
+print(daftar_buku_baru)
 
-# harus ditambah ini ([:]) untuk benar2 baru
+# 7. list lama dihapus, list baru tetep exist
+print('\n7. list lama dihapus, list baru tetep exist')
+del daftar_buku[:]
+for i in range(0, len(daftar_buku_baru)):
+    print(daftar_buku_baru[i])
 
-# list lama dihapus, list baru tetep exist
+# 8. membuat list comprehension genap
+print('\n8. membuat list comprehension genap')
+daftar_buku = [1, 2, 3, 4, 5, 6]
+print(daftar_buku)
+daftar_buku_baru = daftar_buku[1::2]
+print(daftar_buku_baru)
 
-# membuat list comprehension genap
+# 9. membuat list comprehension, buang elemen terakhir
+print('\n9. membuat list comprehension, buang elemen terakhir')
+daftar_buku = [1, 2, 3, 4, 5, 6]
+print(daftar_buku)
+daftar_buku_baru = daftar_buku[0:-1:]
+print(daftar_buku_baru)
 
-# membuat list comprehension, buang elemen terakhir
-
-# membuat list comprehension ganjil
+# 10. membuat list comprehension ganjil
+print('\n10. membuat list comprehension ganjil')
+daftar_buku = [1, 2, 3, 4, 5, 6]
+print(daftar_buku)
+daftar_buku_baru = daftar_buku[0::2]
+print(daftar_buku_baru)
